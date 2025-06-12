dfbe6e18bf5c0db3e196829e3094dfdcd3d5aa49
LinuxBeaver <jaydc@gmx.us>
2025-06-12 17:49:01 -0400

    your message here

--- goo_on_text/compile_goo_text_here/goo.c
+++ goo_on_text/compile_goo_text_here/goo.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ * Credit to Øvind Kolas (pippin) for major GEGL contributions
  * 2022 Beaver goo text with GEGL
  */
 

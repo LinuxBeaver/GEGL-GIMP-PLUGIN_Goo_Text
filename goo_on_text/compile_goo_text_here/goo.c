0811e1c08ac72a800dfea212417d1f58e17de1ec
LinuxBeaver <jaydc@gmx.us>
2025-06-12 17:51:17 -0400

    removing pippins leftover 2006 copyright

--- goo_on_text/compile_goo_text_here/goo.c
+++ goo_on_text/compile_goo_text_here/goo.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Credit to Øvind Kolas (pippin) for major GEGL contributions
+ * Credit to Øyvind Kolås (pippin) for major GEGL contributions
  * 2022 Beaver goo text with GEGL
  */
 
